de5646360148b27896c2244c82983facd7db1bd0
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2022-02-22 15:27:30 +0530

    abc

--- src/routes/route.js
+++ src/routes/route.js
@@ -5,4 +5,13 @@ router.get('/movies', function(req,res){
     let movies = ['p.k','3-Idiots','Zero','Dhoom','Rustom','Antim']
     res.send(movies)
 })
+
+router.get('/movies/:indexNumber', function(req,res){
+    let movies = ['p.k','3-Idiots','Zero','Dhoom','Rustom','Antim']
+    for (const i in movies) {
+        res.send(movies[i])
+        
+    }
+})
+
 module.exports = router;
