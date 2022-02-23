be373d9a0c92048c0a34a03416c07d59c8ba7451
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2022-02-23 13:26:52 +0530

    task

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/hi', function(req,res){
-    res.send("hi")
+    let arr = [1,2,3,5,6,7]
+    let sum =0
+    for(let i=0; i<arr.length; i++){
+        sum +=i
+    }
+    val = sum
+    res.send(val)
 })
 module.exports = router;
