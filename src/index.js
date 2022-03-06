656cd9699284661c4458a67a6cb2b4954a0e8cf2
Sneha-Shrivastav <shrivastavasneha04@gmail.com>
2022-03-07 01:44:14 +0530

    Assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var moment = require('moment');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
@@ -8,13 +9,23 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Sneha4567-DB?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use('/', route);
+  const m1 =   function (req, res, next){
+          let time =  moment().format('DD-MM-YY h:mm:ss a')
+          let ip = req.socket.remoteAddress
+          let path = req.path
+          console.log(time, ip, path)
+          next()
+    }
+
+
+
+app.use(m1);
 
 
 app.listen(process.env.PORT || 3000, function () {
